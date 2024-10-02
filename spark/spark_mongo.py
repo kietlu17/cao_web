6b93cded39904d937eca6197abd37e1694ae4b9e
kietlu17 <kietnguyen17042004@gmail.com>
2024-10-02 17:13:23 +0700

    spark- docker

---
+++ spark/spark_mongo.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+# Tạo SparkSession với MongoDB Spark Connector
+spark = SparkSession.builder \
+    .appName("SparkMongoDBConnection") \
+    .config("spark.mongodb.read.connection.uri", "mongodb://root:rootpassword@mongo:27017/dbmycrawler.tblunitop") \
+    .config("spark.mongodb.write.connection.uri", "mongodb://root:rootpassword@mongo:27017/dbmycrawler.tblunitop_2") \
+    .getOrCreate()
+
+# Đọc dữ liệu từ MongoDB
+df = spark.read.format("mongo").load()
+
+# Hiển thị dữ liệu
+df.show()
+
+# Ví dụ viết dữ liệu trở lại MongoDB
+df.write.format("mongo").mode("append").save()
+
+# Kết thúc Spark session
+spark.stop()
